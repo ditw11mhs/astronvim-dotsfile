02a6c0f7e7c6b776919e6df77202250b2e0de437
Aditya Wardianto <aditya.wardianto@zero-one-group.com>
2023-04-08 11:05:03 +0700

    .

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,13 +1,12 @@
 return {
   { "wakatime/vim-wakatime",   lazy = false },
-  { "pearofducks/ansible-vim", lazy = false },
+  { "pearofducks/ansible-vim", event = "User Astrofile" },
   {
     "phaazon/hop.nvim",
     branch = "v2", -- optional but strongly recommended
-    config = function()
-      -- you can configure Hop the way you like here; see :h hop-config
-      require("hop").setup { keys = "wersdfuiojklcmvn" }
-    end,
+    opts = {
+      keys = "wersdfuiojklcmvn",
+    },
     cmd = "HopWord",
   },
 }
