22fbb76fadd04c9bda102d989a6aa4c60a46e46a
Aditya Wardianto <aditya.wardianto@zero-one-group.com>
2023-04-08 10:11:02 +0700

    init: initial setup

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,22 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.tokyonight" },
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.editing-support.dial-nvim" },
+  { import = "astrocommunity.editing-support.neogen" },
+  {
+    "danymat/neogen",
+    opts = { languages = { python = { template = { annotation_convention = "google_docstrings" } } } },
+  },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
 }
